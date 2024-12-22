b324e610fbb0d4281638977faba60c65cca03beb
Toast <ThatOneToast@proton.me>
2024-12-22 14:38:17 -0600

    update

---
+++ dice-derive/target/debug/build/proc-macro2-a54641bc2a3a7fcb/build_script_build-a54641bc2a3a7fcb.d
@@ -0,0 +1,5 @@
+/Users/austinaleshire/Projects/Dice/dice-derive/target/debug/build/proc-macro2-a54641bc2a3a7fcb/build_script_build-a54641bc2a3a7fcb: /Users/austinaleshire/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
+
+/Users/austinaleshire/Projects/Dice/dice-derive/target/debug/build/proc-macro2-a54641bc2a3a7fcb/build_script_build-a54641bc2a3a7fcb.d: /Users/austinaleshire/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
+
+/Users/austinaleshire/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs:
